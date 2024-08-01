c99e30cda11fae1de00f8f68a0bafd4f02244ba6
Sachin Kishan <sachinkishan02@gmail.com>
2024-08-01 23:50:22 +0530

    single iteration

--- apps/SCP/scp.cu
+++ apps/SCP/scp.cu
@@ -28,7 +28,10 @@ int main(int argc, char** argv)
     DenseMatrix<cuComplex> eb(rx, number_of_vertices, 1);
     DenseMatrix<cuComplex> u(rx, number_of_vertices, 1);
     DenseMatrix<cuComplex> T3(rx, number_of_vertices, 1);
+    DenseMatrix<cuComplex> T1(rx, number_of_vertices, 1);
+    DenseMatrix<cuComplex> y(rx, number_of_vertices, 1);
     SparseMatrix<cuComplex> B(rx);
+    SparseMatrix<cuComplex> L(rx);
 
     rx.for_each_vertex
     (rxmesh::DEVICE,[B, eb, boundaryVertices] __device__(const rxmesh::VertexHandle vh) mutable
@@ -45,25 +48,27 @@ int main(int argc, char** argv)
     // S = [B- (1/Vb) * ebebT];
 
     cuComplex T2 = eb.dot(u);
-    
+
+    B.multiply(u, T1);
+
+    eb.multiply(T2);
+
     rx.for_each_vertex(
         rxmesh::DEVICE,
-        [u, eb, B,T3,T2] __device__(
+        [eb, B,T3,T2,T1] __device__(
             const rxmesh::VertexHandle vh) mutable 
         {
-            cuComplex T1 = make_cuComplex(B(vh, vh).x * u(vh, 0).x,
-                                          B(vh, vh).y * u(vh, 0).y);
-
-            
-            T3(vh,0)= cuCsubf(T1,cuCmulf(T2, eb(vh, 0)));
+            T3(vh, 0) = cuCsubf(T1(vh,0), eb(vh,0));
 
         });
 
-    
+    L.pre_solve(PermuteMethod::NSTDIS);
+    L.solve(T3, y); //Ly=T3
 
-    
+    y.multiply(1/y.norm2());
+    u.copy_from(y);
 
-
+        
 
     rx.get_polyscope_mesh()->addVertexScalarQuantity("vBoundary", boundaryVertices);
 
