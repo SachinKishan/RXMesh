27050160f2fbeb487c49f5952041d16be3301f89
Sachin Kishan <sachinkishan02@gmail.com>
2024-08-01 20:10:18 +0530

    error prone code for allocating complex numbers

--- apps/SCP/scp.cu
+++ apps/SCP/scp.cu
@@ -6,6 +6,9 @@
 using namespace rxmesh;
 
 
+
+
+
 int main(int argc, char** argv)
 {
     Log::init();
@@ -13,7 +16,26 @@ int main(int argc, char** argv)
     const uint32_t device_id = 0;
     cuda_query(device_id);
 
-    RXMeshStatic rx(STRINGIFY(INPUT_DIR) "sphere3.obj");
+    RXMeshStatic rx(STRINGIFY(INPUT_DIR) "bunnyhead.obj");
+
+    auto boundaryVertices = rx.add_vertex_attribute<int>("boundaryVertices", 1);
+    
+    rx.get_boundary_vertices(*boundaryVertices);
+
+    DenseMatrix<cuComplex> eb(rx, rx.get_num_vertices(), 1);
+
+    rx.for_each_vertex
+    (rxmesh::DEVICE,[eb] __device__(const rxmesh::VertexHandle vh) 
+    {
+        eb(vh, 0) = make_cuComplex(1.0f, 1.0f);
+
+    });
+
+    //vertex_color.move(rxmesh::DEVICE, rxmesh::HOST);
+
+    rx.get_polyscope_mesh()->addVertexScalarQuantity("vBoundary", *boundaryVertices);
+
+
 
 #if USE_POLYSCOPE
     polyscope::show();
